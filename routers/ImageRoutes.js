cf45c0910df5ee86f581f142dc3e12a01af9c30f
ramzan1813 <ramzanniazi85@outlook.com>
2023-05-06 11:50:23 +0500

    converted to static

--- routers/ImageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { img } = require("../controllers/ImageController");
-const route = express.Router();
-
-route.post("/Image-generation", img);
-
-module.exports = route;
